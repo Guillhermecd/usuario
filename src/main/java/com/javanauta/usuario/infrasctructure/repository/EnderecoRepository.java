4bff2f15ce9484f87d40e939d769520ab618914f
Guilherme Diniz <guiilhermecd@gmail.com>
2025-11-25 17:15:32 -0300

    WIP: alterações locais

--- src/main/java/com/javanauta/usuario/infrasctructure/repository/EnderecoRepository.java
+++ src/main/java/com/javanauta/usuario/infrasctructure/repository/EnderecoRepository.java
@@ -1,6 +1,6 @@
 package com.javanauta.usuario.infrasctructure.repository;
 
-import com.javanauta.aprendendospring.infrastructure.entity.Endereco;
+import com.javanauta.usuario.infrasctructure.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/javanauta/usuario/infrasctructure/repository/TelefoneRepository.java
+++ src/main/java/com/javanauta/usuario/infrasctructure/repository/TelefoneRepository.java
@@ -1,6 +1,6 @@
 package com.javanauta.usuario.infrasctructure.repository;
 
-import com.javanauta.aprendendospring.infrastructure.entity.Telefone;
+import com.javanauta.usuario.infrasctructure.entity.Telefone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
